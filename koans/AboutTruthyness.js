9a583259fe0b506a23bfbe1dd96da136ee6c8a9c
Sonia Orive <isavo70@hotmail.com>
2022-11-26 19:55:29 +0100

    About Truthyness

--- koans/AboutTruthyness.js
+++ koans/AboutTruthyness.js
@@ -2,21 +2,21 @@ describe("About Truthyness", () => {
 
   it("truthyness of positive numbers", function() {
     var oneIsTruthy = 1 ? true : false;
-    expect(FILL_ME_IN).withContext('is one truthy?').toEqual(oneIsTruthy);
+    expect(true).withContext('is one truthy?').toEqual(oneIsTruthy);
   });
 
   it("truthyness of negative numbers", function() {
     var negativeOneIsTruthy = -1 ? true : false;
-    expect(FILL_ME_IN).withContext('is -1 truthy?').toEqual(negativeOneIsTruthy);
+    expect(true).withContext('is -1 truthy?').toEqual(negativeOneIsTruthy);
   });
 
   it("truthyness of zero", function() {
     var zeroIsTruthy = 0 ? true : false;
-    expect(FILL_ME_IN).withContext('is 0 truthy?').toEqual(zeroIsTruthy);
+    expect(false).withContext('is 0 truthy?').toEqual(zeroIsTruthy);
   });
 
   it("truthyness of null", function() {
     var nullIsTruthy = null ? true : false;
-    expect(FILL_ME_IN).withContext('is null truthy?').toEqual(nullIsTruthy);
+    expect(false).withContext('is null truthy?').toEqual(nullIsTruthy);
   });
 });
