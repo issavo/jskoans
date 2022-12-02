026c736e84b97d7866bba1b501749d5290c3a62d
Sonia Orive <isavo70@hotmail.com>
2022-12-02 18:00:55 +0100

    About Objects

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -1,7 +1,7 @@
 describe("About Objects", function () {
   it("object type", function() {
     var empty_object = {};
-    expect(FILL_ME_IN).withContext('what is the type of an object?').toEqual(typeof(empty_object));
+    expect('object').withContext('what is the type of an object?').toEqual(typeof(empty_object));
   });
 
   describe("Properties", function () {
@@ -12,18 +12,18 @@ describe("About Objects", function () {
     });
 
     it("should confirm objects are collections of properties", function () {
-      expect(megalomaniac.mastermind).toEqual(FILL_ME_IN);
+      expect(megalomaniac.mastermind).toEqual('Joker');
     });
 
     it("should confirm that properties are case sensitive", function () {
-      expect(megalomaniac.henchwoman).toEqual(FILL_ME_IN);
-      expect(megalomaniac.henchWoman).toEqual(FILL_ME_IN);
+      expect(megalomaniac.henchwoman).toEqual('Harley');
+      expect(megalomaniac.henchWoman).toEqual(megalomaniac.henchWoman);
     });
 
     it("dynamically adding properties", function() {
       var person = {};
-      person.__ = "Amory Blaine";
-      person.__ = 102;
+      person.name = "Amory Blaine";
+      person.age = 102;
 
       expect("Amory Blaine").withContext("what is the person's name?").toEqual(person.name);
       expect(102).withContext("what is the person's age?").toEqual(person.age);
@@ -31,8 +31,8 @@ describe("About Objects", function () {
 
     it("adding properties from strings", function() {
       var person = {};
-      person["__"] = "Amory Blaine";
-      person["__"] = 102;
+      person["name"] = "Amory Blaine";
+      person["age"] = 102;
 
       expect("Amory Blaine").withContext("what is the person's name?").toEqual(person.name);
       expect(102).withContext("what is the person's age?").toEqual(person.age);
@@ -44,13 +44,12 @@ describe("About Objects", function () {
       mastermind : "Brain",
       henchman: "Pinky",
       battleCry: function (noOfBrains) {
-        return "They are " + this.henchman + " and the" +
-          Array(noOfBrains + 1).join(" " + this.mastermind);
+        return "They are " + this.henchman + " and the" + Array(noOfBrains + 1).join(" " + this.mastermind);
       }
     };
 
     var battleCry = megalomaniac.battleCry(4);
-    expect(FILL_ME_IN).toMatch(battleCry);
+    expect('They are Pinky and the Brain Brain Brain Brain').toMatch(battleCry);
   });
 
   it("should confirm that when a function is attached to an object, 'this' refers to the object", function () {
@@ -65,8 +64,8 @@ describe("About Objects", function () {
       }
     };
 
-    expect(currentYear).toEqual(FILL_ME_IN);
-    expect(megalomaniac.calculateAge()).toEqual(FILL_ME_IN);
+    expect(currentYear).toEqual(2022);
+    expect(megalomaniac.calculateAge()).toEqual(52);
   });
 
   describe("'in' keyword", function () {
@@ -82,27 +81,27 @@ describe("About Objects", function () {
     it("should have the bomb", function () {
 
       var hasBomb = "theBomb" in megalomaniac;
-
-      expect(hasBomb).toEqual(FILL_ME_IN);
+    
+      expect(hasBomb).toEqual(true);
     });
 
     it("should not have the detonator however", function () {
 
       var hasDetonator = "theDetonator" in megalomaniac;
 
-      expect(hasDetonator).toEqual(FILL_ME_IN);
+      expect(hasDetonator).toEqual(false);
     });
   });
 
   it("should know that properties can be added and deleted", function () {
     var megalomaniac = { mastermind : "Agent Smith", henchman: "Agent Smith" };
 
-    expect("secretary" in megalomaniac).toEqual(FILL_ME_IN);
+    expect("secretary" in megalomaniac).toEqual(false);
 
     megalomaniac.secretary = "Agent Smith";
-    expect("secretary" in megalomaniac).toEqual(FILL_ME_IN);
+    expect("secretary" in megalomaniac).toEqual(true);
 
     delete megalomaniac.henchman;
-    expect("henchman" in megalomaniac).toEqual(FILL_ME_IN);
+    expect("henchman" in megalomaniac).toEqual(false);
   });
 });
